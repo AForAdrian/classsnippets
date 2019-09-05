063f719fb0990a7b624bbb823165d196fbeea6fe
Adrian Harman <45378375+AdrianHarman@users.noreply.github.com>
2019-09-05 17:03:41 +0300

    Add files via upload

---
+++ ReadInput.java
@@ -0,0 +1,28 @@
+
+package readinput;
+
+
+import java.util.Scanner;
+
+public class ReadInput{
+
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        int positive=0;
+        int negative=0;
+        int sum=0;
+        int x;
+        do{
+            System.out.println("Enter the values to average:");
+            x = input.nextInt();
+            if(x>0){
+                positive++;
+            }
+            else{
+                negative++;
+            }
+            sum+=x;
+        }while(x!=0);
+        System.out.println(sum/(positive+negative));
+    }
+}
