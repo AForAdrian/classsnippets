ffa1aae8666757ab51e698507d9b56664c0f7753
Adrian Harman <45378375+AdrianHarman@users.noreply.github.com>
2019-09-06 10:40:56 +0300

    Update elmAdd.java

--- elmAdd.java
+++ elmAdd.java
@@ -6,7 +6,7 @@ public class areavol{
         int num = input.nextInt();
         int sum = 0;
 
-        while(num>10){
+        while(num>0){
             x = num % 10;
             sum += x;
             num = num/10;
