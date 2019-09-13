72b91e03de3f9214bea8d5146e3815460b0fc84b
Adrian Harman <45378375+AdrianHarman@users.noreply.github.com>
2019-09-13 10:02:27 +0300

    Update mathEqn.java

--- mathEqn.java
+++ mathEqn.java
@@ -17,15 +17,15 @@ public class mathEqn {
         System.out.println("value of c:");
         c = read.nextInt();
         
-        double determinant = Math.pow(b,2) - (4*a*c);
+        double didscriminant = Math.pow(b,2) - (4*a*c);
         
-        if(determinant > 0){
-            aEqn1 =  (-b + Math.sqrt(determinant)) / (2 * a);
-            aEqn2 =  (-b - Math.sqrt(determinant)) / (2 * a); 
+        if(didscriminant > 0){
+            aEqn1 =  (-b + Math.sqrt(didscriminant)) / (2 * a);
+            aEqn2 =  (-b - Math.sqrt(didscriminant)) / (2 * a); 
             System.out.println("R1 = " + aEqn1 + " and R2 = " + aEqn2);
         }
-        else if ( determinant == 0){
-            aEqn1 =  (b + Math.sqrt(determinant)) / (2 * a);
+        else if ( didscriminant == 0){
+            aEqn1 =  (b + Math.sqrt(didscriminant)) / (2 * a);
             System.out.println("R = " + aEqn1);
         }
         else{
