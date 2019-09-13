cdd2e814da4fde5eb9cecb950fec21a05a3096ed
Adrian Harman <45378375+AdrianHarman@users.noreply.github.com>
2019-09-13 09:03:54 +0300

    Update and rename Number2 lab 2.0.java to textFormat.java

--- textFormat.java
+++ textFormat.java
@@ -1,9 +1,4 @@
-
-package number.pkg2;
-
-public class Number2lab{
-
-   
+public class textFormat{   
     public static void main(String[] args) {
         System.out.println("Kilograms       Pounds");
         System.out.println("1\t         2.2\t");
@@ -11,7 +6,5 @@ public class Number2lab{
         System.out.println("...\t");
         System.out.println("197\t         433.4\t");
         System.out.println("199\t         437.8\t");
-        
     }
-    
 }
