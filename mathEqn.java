42cc5ee89f89ff3f3a081db39f69321c780701e2
Adrian Harman <45378375+AdrianHarman@users.noreply.github.com>
2019-09-13 10:10:57 +0300

    Update mathEqn.java

--- mathEqn.java
+++ mathEqn.java
@@ -17,21 +17,20 @@ public class mathEqn {
         System.out.println("value of c:");
         c = read.nextInt();
         
-        double didscriminant = Math.pow(b,2) - (4*a*c);
+        double discriminant = Math.pow(b,2) - (4*a*c);
         
-        if(didscriminant > 0){
-            aEqn1 =  (-b + Math.sqrt(didscriminant)) / (2 * a);
-            aEqn2 =  (-b - Math.sqrt(didscriminant)) / (2 * a); 
+        if(discriminant > 0){
+            aEqn1 =  (-b + Math.sqrt(discriminant)) / (2 * a);
+            aEqn2 =  (-b - Math.sqrt(discriminant)) / (2 * a); 
             System.out.println("R1 = " + aEqn1 + " and R2 = " + aEqn2);
         }
-        else if ( didscriminant == 0){
-            aEqn1 =  (b + Math.sqrt(didscriminant)) / (2 * a);
+        else if ( discriminant == 0){
+            aEqn1 =  (b + Math.sqrt(discriminant)) / (2 * a);
             System.out.println("R = " + aEqn1);
         }
         else{
-            System.out.println(" “The equation has no real roots” ");
+            System.out.println(" “The equation has no real roots” ");
         }
         }
     }
     
-}
